32513f456579f521d8b2e813d34a210ac0d9278d
unknown <jkvapil6@gmail.com>
2020-07-02 21:10:21 +0200

    Updates

---
+++ precache-manifest.ce07d5c1710281c0ba04c05ba96dfa6e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ee6515c019770612e754d51026b9d1bf",
+    "url": "/jkvapil6.github.io/weather/index.html"
+  },
+  {
+    "revision": "12a55de51af537d2681d",
+    "url": "/jkvapil6.github.io/weather/static/css/main.32a9b530.chunk.css"
+  },
+  {
+    "revision": "91e7eff15b60bca17c6b",
+    "url": "/jkvapil6.github.io/weather/static/js/2.05911bde.chunk.js"
+  },
+  {
+    "revision": "aa754ba9f73776ab5291549b6092b0f0",
+    "url": "/jkvapil6.github.io/weather/static/js/2.05911bde.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "12a55de51af537d2681d",
+    "url": "/jkvapil6.github.io/weather/static/js/main.93806661.chunk.js"
+  },
+  {
+    "revision": "f9b8668d59d407f31239",
+    "url": "/jkvapil6.github.io/weather/static/js/runtime-main.0f15ec45.js"
+  }
+]);
